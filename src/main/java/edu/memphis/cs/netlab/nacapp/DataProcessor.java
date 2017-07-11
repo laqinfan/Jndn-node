ac3b5e0bdb78007022c3df671f30ed59e515e33b
lpimem <lpi@memphis.edu>
2017-07-11 11:22:07 -0500

    init

---
+++ src/main/java/edu/memphis/cs/netlab/nacapp/DataProcessor.java
@@ -0,0 +1,14 @@
+package edu.memphis.cs.netlab.nacapp;
+
+import net.named_data.jndn.Data;
+
+/**
+ * Description:
+ * <p>
+ * Author: lei
+ */
+
+public interface DataProcessor {
+  Data process(Data d) throws Exception;
+}
+
---
+++ src/main/java/edu/memphis/cs/netlab/nacapp/NACConsumers.java
@@ -0,0 +1,26 @@
+package edu.memphis.cs.netlab.nacapp;
+
+import net.named_data.jndn.Face;
+import net.named_data.jndn.Name;
+import net.named_data.jndn.encrypt.Consumer;
+import net.named_data.jndn.encrypt.ConsumerDb;
+import net.named_data.jndn.encrypt.Sqlite3ConsumerDb;
+import net.named_data.jndn.security.KeyChain;
+
+
+
+/**
+ * Description:
+ * <p>
+ * Author: lei
+ */
+
+public abstract class NACConsumers {
+  public static Consumer newConsumer(Face f, KeyChain kc, Name group, Name consumer, String dbFilePath) {
+    try {
+      return new Consumer(f, kc, group, consumer, new Sqlite3ConsumerDb(dbFilePath));
+    } catch (ConsumerDb.Error error) {
+      throw new RuntimeException(error);
+    }
+  }
+}
